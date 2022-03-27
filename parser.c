7159e6a24bd3d8ebfb06f2fcaab63b557bcf0e55
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2022-03-27 23:07:58 +0100

    ADD parser

---
+++ parser.c
@@ -0,0 +1,145 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+/**
+ * Notational conventions:
+ *
+ * SR: SR1, SR2: Source Register; one of R0..R7 which specifies the register from which a source operand is obtained
+ *
+ * DR: Destination Register; one of R0..R7, which specifies which register the result of an instruction should be written to
+ *
+ * imm5: A 5-bit immediate value; bits [4:0] of an instruction when used as a literal (immediate) value.
+ * Taken as a 5-bit, 2’s complement integer, it is sign-extended to 16 bits before it is used. Range: −16..15
+ *
+ */
+
+
+void error_exit(const char *format, const char *text) {
+    printf(format, text);
+    exit(EXIT_FAILURE);
+}
+
+int is_register(char *token) {
+    if(strcmp(token, "R1") == 0)
+    {
+        return 1;
+    }
+    else if(strcmp(token, "R2") == 0)
+    {
+        return 2;
+    }
+    else if(strcmp(token, "R3") == 0)
+    {
+        return 3;
+    }
+    else if(strcmp(token, "R4") == 0)
+    {
+        return 4;
+    }
+    else if(strcmp(token, "R5") == 0)
+    {
+        return 5;
+    }
+    else if(strcmp(token, "R6") == 0)
+    {
+        return 6;
+    }
+    else if(strcmp(token, "R7") == 0)
+    {
+        return 7;
+    }
+    return -1;
+}
+
+bool is_imm5(char *token) {
+    int imm5;
+    char first_ch = *token;
+    if(first_ch == '#') { //decimal literal
+        imm5 = atoi(first_ch + 1);
+        return imm5 >= -16 && imm5 <= 15;
+    }
+    else if(first_ch == 'x') { //hex literal
+        imm5 = sscanf(first_ch + 1, "x");
+        return imm5 >= -16 && imm5 <= 15;
+    }
+    else
+        return false;
+}
+
+/**
+ * @brief
+ *
+ * Assembler formats:
+ * ADD DR,SR1,SR2
+ * ADD DR,SR1,imm5
+ *
+ * @param instr ADD instruction
+ * @return char* binary representation
+ */
+char *parse_add(char *instr) {
+
+    //READING INSTRUCTION TOKENS
+    char *tokens[4];
+    int i = 0;
+    char *delimiters = " ,";
+    char *pch = strtok(instr, delimiters);
+    while(pch != NULL) {
+        if(i > 3)
+        {
+            error_exit("unexpected token in instruction %s", instr);
+        }
+
+        tokens[i++] = pch;
+        pch = strtok(NULL, delimiters);
+    }
+
+    //VALIDATING TOKENS
+    if(strcmp(tokens[0], "ADD"))
+    {
+        //this should not happen
+        error_exit("expected ADD but found %s", tokens[0]);
+    }
+
+    if(!is_register(tokens[1]))
+    {
+        error_exit("expected register but found %s", tokens[1]);
+    }
+
+    if(!is_register(tokens[2]))
+    {
+        error_exit("expected register but found %s", tokens[1]);
+    }
+
+    if(!is_register(tokens[3]) && !is_imm5(tokens[3]))
+    {
+        error_exit("expected register or imm5 but found %s", tokens[1]);
+    }
+
+    //CONVERTING TO BINARY REPRESENTATION
+    unsigned int ret;
+
+    //ops code: 0001
+    ret = 1 << 11;
+
+    //DR
+
+
+
+
+    return "";
+}
+
+
+char *parse_line(char *line) {
+
+    return "";
+}
+
+#ifdef FAB_MAIN
+int main(int argc, char const *argv[]) {
+
+}
+#endif
