c6ad8358d3f232a1ac98094f8223931c23d9c926
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2022-07-11 23:39:03 +0100

    add test fix

--- test/parser_add_test.c
+++ test/parser_add_test.c
@@ -5,112 +5,78 @@
 #include <stdbool.h>
 #include "../include/lc3.h"
 
-void test_add_SR2(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,R1,R2";
-    uint16_t machine_instr = parse_add(asm_instr);
+void test_add_SR2(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R0","R1","R2");
     char *bytes = (char *)&machine_instr;
     //assert order is flipped because of little-endian arch
     assert_int_equal(bytes[0], 66);
     assert_int_equal(bytes[1], 16);
 }
 
-void test_add_imm5_decimal(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,R1,#13";
-    uint16_t machine_instr = parse_add(asm_instr);
+void test_add_imm5_decimal(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R0","R1","#13");
     char *bytes = (char *)&machine_instr;
     //assert order is flipped because of little-endian arch
     assert_int_equal(bytes[0], 109);
     assert_int_equal(bytes[1], 16);
 }
 
-void test_add_imm5_hex(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,R1,xa";
-    uint16_t machine_instr = parse_add(asm_instr);
+void test_add_imm5_hex(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R0","R1","xa");
     char *bytes = (char *)&machine_instr;
     //assert order is flipped because of little-endian arch
     assert_int_equal(bytes[0], 106);
     assert_int_equal(bytes[1], 16);
 }
 
-void test_add_wrong_register_DR(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R8,R1,xa";
-    uint16_t machine_instr = parse_add(asm_instr);
-
+void test_add_wrong_register_DR(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R8","R1","xa");
     assert_int_equal(machine_instr, 0);
     assert_string_equal(errdesc, "expected register but found R8\n");
 }
 
-void test_add_wrong_register_SR1(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,SR1,xa";
-    uint16_t machine_instr = parse_add(asm_instr);
-
+void test_add_wrong_register_SR1(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R0","SR1","xa");
     assert_int_equal(machine_instr, 0);
     assert_string_equal(errdesc, "expected register but found SR1\n");
 }
 
-void test_add_wrong_imm5_too_big_dec(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,R1,#16";
-    uint16_t machine_instr = parse_add(asm_instr);
-
+void test_add_wrong_imm5_too_big_dec(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R0","R1","#16");
     assert_int_equal(machine_instr, 0);
     assert_string_equal(errdesc, "immediate 16 is outside the range [-16,15]\n");
 }
 
-void test_add_wrong_imm5_too_small_dec(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,R1,#-17";
-    uint16_t machine_instr = parse_add(asm_instr);
-
+void test_add_wrong_imm5_too_small_dec(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R0","R1","#-17");
     assert_int_equal(machine_instr, 0);
     assert_string_equal(errdesc, "immediate -17 is outside the range [-16,15]\n");
 }
 
-void test_add_wrong_imm5_too_big_hex(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,R1,xf1";
-    uint16_t machine_instr = parse_add(asm_instr);
-
+void test_add_wrong_imm5_too_big_hex(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R0","R1","xf1");
     assert_int_equal(machine_instr, 0);
     assert_string_equal(errdesc, "immediate f1 is outside the range [-16,15]\n");
 }
 
-void test_add_wrong_imm5_too_small_hex(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,R1,x-f2";
-    uint16_t machine_instr = parse_add(asm_instr);
-
+void test_add_wrong_imm5_too_small_hex(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R0","R1","x-f2");
     assert_int_equal(machine_instr, 0);
     assert_string_equal(errdesc, "immediate -f2 is outside the range [-16,15]\n");
 }
 
-void test_add_wrong_imm5_format(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,R1,0";
-    uint16_t machine_instr = parse_add(asm_instr);
-
+void test_add_wrong_imm5_format(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R0","R1","0");
     assert_int_equal(machine_instr, 0);
     assert_string_equal(errdesc, "immediate 0 must be decimal or hex\n");
 }
 
-void test_add_wrong_imm5_number(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,R1,#y";
-    uint16_t machine_instr = parse_add(asm_instr);
-
+void test_add_wrong_imm5_number(void  __attribute__ ((unused)) **state) {    
+    uint16_t machine_instr = parse_add("R0","R1","#y");
     assert_int_equal(machine_instr, 0);
     assert_string_equal(errdesc, "immediate #y is not a numeric value\n");
 }
 
-void test_add_wrong_instruction(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "AND R0,R1,R2";
-    uint16_t machine_instr = parse_add(asm_instr);
-
-    assert_int_equal(machine_instr, 0);
-    assert_string_equal(errdesc, "expected ADD but found AND\n");
-}
-
-void test_add_wrong_element_in_instruction(void  __attribute__ ((unused)) **state) {
-    char asm_instr[] = "ADD R0,R1,R2,R3";
-    uint16_t machine_instr = parse_add(asm_instr);
-
-    assert_int_equal(machine_instr, 0);
-    assert_string_equal(errdesc, "unexpected token in ADD instruction\n");
-}
 
 int main(int argc, char const *argv[]) {
     const struct CMUnitTest tests[] = {
@@ -124,9 +90,7 @@ int main(int argc, char const *argv[]) {
         cmocka_unit_test(test_add_wrong_imm5_too_big_hex),
         cmocka_unit_test(test_add_wrong_imm5_too_small_hex),
         cmocka_unit_test(test_add_wrong_imm5_format),
-        cmocka_unit_test(test_add_wrong_imm5_number),
-        cmocka_unit_test(test_add_wrong_instruction),
-        cmocka_unit_test(test_add_wrong_element_in_instruction)
+        cmocka_unit_test(test_add_wrong_imm5_number)        
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
