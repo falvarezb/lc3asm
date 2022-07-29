2945c38c0773c237b7502188b9f4e8be56493c8e
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2022-07-29 16:13:46 +0100

    refactoring

--- test/assembler_test.c
+++ test/assembler_test.c
@@ -23,30 +23,6 @@ static void assert_symbol_table(const char *label, size_t num_instruction) {
     assert_int_equal(node->val, num_instruction);    
 }
 
-// static void run_second_pass_test(char *asm_file_name, char *expected_obj_file_name, char *actual_obj_file_name) {
-//     second_pass_parse(asm_file_name, actual_obj_file_name);
-
-//     FILE *expected_obj_file = fopen(expected_obj_file_name, "r");
-//     FILE *actual_obj_file = fopen(actual_obj_file_name, "r");
-
-//     char buf_expected[2];
-//     char buf_actual[2];
-//     size_t num_lines = 1;
-
-//     size_t read;
-//     while((read = fread(buf_expected, 1, 2, expected_obj_file)) == 2) {
-//         read = fread(buf_actual, 1, 2, actual_obj_file);
-//         printf("line checked: %zu\n", num_lines);
-//         assert_int_equal(read, 2);
-//         assert_true(buf_expected[0] == buf_actual[0] && buf_expected[1] == buf_actual[1]);
-//         num_lines++;
-//     }
-
-//     assert_int_equal(read, fread(buf_actual, 1, 2, actual_obj_file));
-//     fclose(expected_obj_file);
-//     fclose(actual_obj_file);
-// }
-
 static void run_assemble_test(char *asm_file_name, char *expected_obj_file_name, char *actual_obj_file_name) {
     exit_t result = assemble(asm_file_name);
     if(result.code) {
@@ -76,17 +52,6 @@ static void run_assemble_test(char *asm_file_name, char *expected_obj_file_name,
     fclose(expected_obj_file);
     fclose(actual_obj_file);
 }
-
-///////////////////////////////////////////////////
-
-// static void test_second_pass_without_labels_t1(void  __attribute__((unused)) **state) {
-//     run_second_pass_test("./test/t1.asm", "./test/t1.expected.obj", "./test/t1.obj");
-// }
-
-// static void test_second_pass_with_labels_t2(void  __attribute__((unused)) **state) {
-//     add("LABEL", 0x3003);
-//     run_second_pass_test("./test/t2.asm", "./test/t2.expected.obj", "./test/t2.obj");
-// }
 
 static void test_symbol_table_t2(void  __attribute__((unused)) **state) {
     assemble("./test/t2.asm");
