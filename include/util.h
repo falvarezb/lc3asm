8d6ac2e0080c2eaed99d6bfa7f7c509cf4f781a1
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-02-25 17:57:52 +0000

    adding __printflike

--- include/util.h
+++ include/util.h
@@ -17,6 +17,7 @@
 #define MAX_NUM_TOKENS 200
 extern char errdesc[];
 
+// passing elipsis around from a function to another is only possible with macros
 #define failure(exit_code, format, ...) do_exit(exit_code, format, __VA_ARGS__)
 
 typedef struct {
@@ -29,14 +30,14 @@ typedef struct {
     char *after;
 } split_t;
 
-void error_exit(const char *format, ...);
+void error_exit(const char *format, ...) __printflike(1, 2);
 char *bin(unsigned int decimal);
-int seterrdesc(char *format, ...);
+int seterrdesc(char *format, ...) __printflike(1, 2);
 void clearerrdesc();
 bool strtolong(char *str, long *num, int base);
 char **split_tokens(char *str, int *num_tokens, const char *delimiters);
 char *split_by_last_delimiter(char *str, char delimiter);
-exit_t do_exit(int exit_code, char *format, ...);
+exit_t do_exit(int exit_code, char *format, ...) __printflike(2, 3);
 exit_t success();
 void free_err(exit_t err);
 
